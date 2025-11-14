dcb206d45d907414eeb5cac89ca49aee1e7a528a
Davi-design <137007200+Davi-design@users.noreply.github.com>
2025-11-13 21:54:28 -0300

    Projeto Finalizado!

--- projeto-concessionaria-main/listar-funcionario.php
+++ projeto-concessionaria-main/listar-funcionario.php
@@ -19,19 +19,17 @@
         print "</tr>";
         while ($row = $res->fetch_object()) {
             print "<tr>";
-            print "<td>" . $row->id_funcionario . "</td>";
-            print "<td>" . $row->nome_funcionario . "</td>";
-            
-            // CORREÇÃO 1: Garante que a propriedade existe antes de tentar acessá-la
+            print "<td>" . htmlspecialchars($row->id_funcionario ?? '') . "</td>";
+            print "<td>" . htmlspecialchars($row->nome_funcionario ?? '') . "</td>";
             print "<td>" . htmlspecialchars($row->cpf_funcionario ?? '') . "</td>";
             print "<td>" . htmlspecialchars($row->email_funcionario ?? '') . "</td>";
-
-            // CORREÇÃO 2: Usa 'telefone_funcionario' em vez de 'fone_funcionario'
             print "<td>" . htmlspecialchars($row->telefone_funcionario ?? '') . "</td>";
-            
+
+            // Links usam 'id' para compatibilidade com salvar-funcionario.php
+            $id = (int)($row->id_funcionario ?? 0);
             print "<td>
-                        <button onclick=\"location.href='?page=editar-funcionario&id=".$row->id_funcionario."';\" class='btn btn-success'>Editar</button>
-                        <button onclick=\"if(confirm('Tem certeza que deseja excluir?')){location.href='?page=salvar-funcionario&acao=excluir&id=".$row->id_funcionario."';}else{false;}\" class='btn btn-danger'>Excluir</button>
+                        <button onclick=\"location.href='?page=editar-funcionario&id={$id}';\" class='btn btn-success'>Editar</button>
+                        <button onclick=\"if(confirm('Tem certeza que deseja excluir?')){location.href='?page=salvar-funcionario&acao=excluir&id={$id}';}else{false;}\" class='btn btn-danger'>Excluir</button>
                    </td>";
             print "</tr>";
         }
